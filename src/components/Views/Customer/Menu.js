338b6d959c1357f40436757dfe79d0969966c8aa
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2024-08-26 12:50:30 +0200

    Update Menu.js

--- src/components/Views/Customer/Menu.js
+++ src/components/Views/Customer/Menu.js
@@ -37,7 +37,7 @@ const Menu = ({ addToCart }) => {
         id: item.id,
         name: item.name,
         description: item.description,
-        image: `http://localhost:8080/ClubCurry/image/getByMenuId/${item.id}`, // Updated image URL
+        image: `http://localhost:8080/ClubCurry/image/getByMenuId/${item.id}`,
         price: item.price,
       });
     });
