88459846ded973a3a2b71b8dac83de99ef20946a
Mohamed Zouheir <126957679+mohamedzouheir@users.noreply.github.com>
2024-10-17 16:58:14 +0200

    Added functionality to Employee Order Summary

--- src/components/Views/GeneralEmployee/Employee.js
+++ src/components/Views/GeneralEmployee/Employee.js
@@ -1,27 +1,21 @@
 // Employee.js
-import React from 'react';
+import React, {useEffect} from 'react';
 import './Employee.css';
 
-const Employee = ({ addToOrder }) => {
-    const products = [
-        { id: 1, name: 'Zinger Burger', price: 150, image: 'path/to/zinger-burger.jpg' },
-        { id: 2, name: 'Pizza', price: 100, image: 'path/to/pizza.jpg' },
-        { id: 3, name: 'Sprite', price: 50, image: 'path/to/sprite.jpg' },
-        { id: 4, name: 'Chicken Tikka', price: 200, image: 'path/to/chicken-tikka.jpg' },
-        { id: 5, name: 'Cheese Lover', price: 120, image: 'path/to/cheese-lover.jpg' },
-        { id: 6, name: 'Double Zinger', price: 180, image: 'path/to/double-zinger.jpg' },
-        { id: 7, name: 'Chicken Burger', price: 130, image: 'path/to/chicken-burger.jpg' },
-        { id: 8, name: 'Beef Kebab', price: 100, image: 'path/to/beef-kebab.jpg' },
-    ];
+
+const Employee = (props) => {
+
+    useEffect(() => {
+    }, []);
 
     return (
         <div className="product-grid">
-            {products.map((product) => (
-                <div key={product.id} className="product-card" onClick={() => addToOrder(product)}>
+            {props.products.map((product) => (product.menuId == props.currentMenuId ? (
+                <div key={product.id} className="product-card" onClick={() => props.handleAddToOrder(product)}>
                     <img src={product.image} alt={product.name} className="product-image" />
                     <h3>{product.name}</h3>
                     <p>Price: {product.price}</p>
-                </div>
+                </div>) : <></>
             ))}
         </div>
     );
